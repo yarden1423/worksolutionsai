d823427173d1464aa31d684059250de756bee2ad
yarden Nazaretsky <yardennaz@gmail.com>
2024-05-22 13:06:18 +0300

    chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Chat from "./components/Chat";
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth>
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite.js example
-        </Typography>
-        <ProTip />
-        <Copyright />
+        <Chat />
       </Box>
     </Container>
   );
