3150d03c8412f91bf316e8995e13b1b207979590
Nimrod W <nimrodwynne@gmail.com>
2024-05-22 13:28:23 +0300

    added example cvs and connected the python to gemini's api

--- gemini-service/main.py
+++ gemini-service/main.py
@@ -1,1 +1,14 @@
-import request
+import gemini
+
+with open("data/cv_examples/agriculture_he.txt", "r", encoding='utf-8') as f:
+    user_cv = f.read()
+    print("sending request to gemini api")
+    response = None
+    try:
+        response = gemini.get_response(user_cv)
+    except Exception as e:
+        print(e)
+    if response is not None:
+        print(response.text)
+    else:
+        print("received no response from the api")
