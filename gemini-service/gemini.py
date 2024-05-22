15f73e49c66ecb4ff551bcce5c0fabb7b392f1f6
Nimrod W <nimrodwynne@gmail.com>
2024-05-22 17:45:07 +0300

    configuring the history of the traits model to use an argument of traits by input

--- gemini-service/gemini.py
+++ gemini-service/gemini.py
@@ -1,9 +1,10 @@
 from fields_model import fields_chat_session
-from traits_model import traits_chat_session
+from traits_model import traits_model
 
 
-def get_user_traits(user_details: str):
-    response = traits_chat_session.send_message(user_details)
+def get_user_traits(user_details: str, traits):
+    m = traits_model(traits=traits)
+    response = m.send_message(user_details)
     return response
 
 
--- gemini-service/main.py
+++ gemini-service/main.py
@@ -29,14 +29,14 @@ def get_user_fields_from_gemini(text_data):
     return response
 
 
-def get_user_traits_from_gemini(text_data):
+def get_user_traits_from_gemini(text_data, traits):
     """
     given a cv from the user, get the traits of the individual
     """
     print("sending request to gemini api")
     response = None
     try:
-        response = gemini.get_user_traits(text_data)
+        response = gemini.get_user_traits(text_data, traits=traits)
     except Exception as e:
         print(e)
     if response is None:
@@ -51,10 +51,8 @@ def process_gemini_json(gemini_str: str):
     return None
 
 
-print("processing traits from json")
-with open("../data/skills/fields_he.json", "r", encoding='utf-8') as f:
-    traits_json = f.read()
-    traits_db = process_traits(traits_json)
+with open("../data/skills/traits_he.txt", "r", encoding='utf-8') as f:
+    traits = f.read()
 
 with open("../data/skills/fields_he.json", "r", encoding='utf-8') as f:
     fields_json = f.read()
@@ -62,7 +60,7 @@ with open("../data/skills/fields_he.json", "r", encoding='utf-8') as f:
 
 with open("../data/cv_examples/manufacturing_he.txt", "r", encoding='utf-8') as f:
     user_cv = f.read()
-    traits_response = get_user_traits_from_gemini(user_cv)
+    traits_response = get_user_traits_from_gemini(user_cv, traits=traits)
     traits = process_gemini_json(traits_response.text)
 
     fields_response = get_user_fields_from_gemini(user_cv)
