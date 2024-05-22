a7ddaec0209d2c8257cdf10ba7bd6c11adb179d3
yarden Nazaretsky <yardennaz@gmail.com>
2024-05-23 00:28:16 +0300

    finish add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,52 +9,6 @@ import { Typography } from "@mui/material";
 import Login from "./pages/Login";
 
 export default function App() {
-  const jobs = [
-    {
-      ID: 1,
-      jobTitle: "נודר נדר",
-      description:
-        "אני מחפש אנשים שרוצים לתת את כל מה שיש להם, אוהבים לעבוד ועובדים על אהבה, מחפשים את האחד ומוכנים לחכות",
-      location: "רעננה",
-      companyName: "Redhat",
-      skills: [
-        { id: 1, name: "Java" },
-        { id: 2, name: "Python" },
-        { id: 3, name: "C++" },
-      ],
-      contactInfo: "0522803670",
-      link: "https://www.youtube.com/watch?v=HxD5La2BlwQ",
-    },
-    {
-      ID: 2,
-      jobTitle: "מקם",
-      description: "I am looking for a software developer",
-      location: "Raanana",
-      companyName: "Redhat",
-      skills: [
-        { id: 1, name: "Java" },
-        { id: 2, name: "Python" },
-        { id: 3, name: "C++" },
-      ],
-      contactInfo: "0522803670",
-      link: "https://www.youtube.com/watch?v=HxD5La2BlwQ",
-    },
-    {
-      ID: 3,
-      jobTitle: "מפתח חקלאי",
-      description: "I am looking for a software developer",
-      location: "Raanana",
-      companyName: "Redhat",
-      skills: [
-        { id: 1, name: "Java" },
-        { id: 2, name: "Python" },
-        { id: 3, name: "C++" },
-      ],
-      contactInfo: "0522803670",
-      link: "https://www.youtube.com/watch?v=HxD5La2BlwQ",
-    },
-  ];
-
   return (
     <BrowserRouter>
       <Typography
@@ -74,7 +28,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cvpage" element={<CVpage />} />
-        <Route path="/workplaces" element={<Suggestion jobs={jobs} />} />
+        <Route path="/workplaces" element={<Suggestion />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
